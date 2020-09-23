962085a159c329d859f11307444cf7a14fe7d3ea
Likeprodingo <42166797+Likeprodingo@users.noreply.github.com>
2020-09-23 21:44:02 +0300

    Fix

--- Assets/Scripts/Joystick.cs
+++ Assets/Scripts/Joystick.cs
@@ -2,51 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 
 public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
-    public float HandleRange
-    {
-        get { return handleRange; }
-        set { handleRange = Mathf.Abs(value); }
-    }
 
-    public float DeadZone
-    {
-        get { return deadZone; }
-        set { deadZone = Mathf.Abs(value); }
-    }
-    public delegate void Move(Vector2 input);
-    public event Move MoveEvent = default; 
     
+    [SerializeField] 
+    private float _handleRange = 1;
+    [SerializeField] 
+    private float _deadZone = 0;
+    [SerializeField] 
+    protected RectTransform _background = null;
+    [SerializeField] 
+    private RectTransform _handle = null;
+    private RectTransform _baseRect = null;
 
-    [SerializeField] private float handleRange = 1;
-    [SerializeField] private float deadZone = 0;
+    private Canvas _canvas;
+    private Camera _cam;
 
-    [SerializeField] protected RectTransform background = null;
-    [SerializeField] private RectTransform handle = null;
-    private RectTransform baseRect = null;
-
-    private Canvas canvas;
-    private Camera cam;
-
-    private Vector2 input = Vector2.zero;
+    private Vector2 _input = Vector2.zero;
+    public delegate void Move(Vector2 input);
+    public event Move MoveEvent = default;
 
     protected virtual void Start()
     {
-        HandleRange = handleRange;
-        DeadZone = deadZone;
-        baseRect = GetComponent<RectTransform>();
-        canvas = GetComponentInParent<Canvas>();
-        if (canvas == null)
+        _baseRect = GetComponent<RectTransform>();
+        _canvas = GetComponentInParent<Canvas>();
+        if (_canvas == null)
             Debug.LogError("The Joystick is not placed inside a canvas");
 
         Vector2 center = new Vector2(0.5f, 0.5f);
-        background.pivot = center;
-        handle.anchorMin = center;
-        handle.anchorMax = center;
-        handle.pivot = center;
-        handle.anchoredPosition = Vector2.zero;
+        _background.pivot = center;
+        _handle.anchorMin = center;
+        _handle.anchorMax = center;
+        _handle.pivot = center;
+        _handle.anchoredPosition = Vector2.zero;
     }
 
     public virtual void OnPointerDown(PointerEventData eventData)
@@ -56,42 +47,42 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoint
 
     public void OnDrag(PointerEventData eventData)
     {
-        cam = null;
-        if (canvas.renderMode == RenderMode.ScreenSpaceCamera)
-            cam = canvas.worldCamera;
+        _cam = null;
+        if (_canvas.renderMode == RenderMode.ScreenSpaceCamera)
+            _cam = _canvas.worldCamera;
 
-        Vector2 position = RectTransformUtility.WorldToScreenPoint(cam, background.position);
-        Vector2 radius = background.sizeDelta / 2;
-        input = (eventData.position - position) / (radius * canvas.scaleFactor);
-        HandleInput(input.magnitude, input.normalized, radius, cam);
-        handle.anchoredPosition = input * radius * handleRange;
-        MoveEvent?.Invoke(input);    
+        Vector2 position = RectTransformUtility.WorldToScreenPoint(_cam, _background.position);
+        Vector2 radius = _background.sizeDelta / 2;
+        _input = (eventData.position - position) / (radius * _canvas.scaleFactor);
+        HandleInput(_input.magnitude, _input.normalized, radius, _cam);
+        _handle.anchoredPosition = _input * radius * _handleRange;
+        MoveEvent?.Invoke(_input);    
     }
 
     protected virtual void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
     {
-        if (magnitude > deadZone)
+        if (magnitude > _deadZone)
         {
             if (magnitude > 1)
-                input = normalised;
+                _input = normalised;
         }
         else
-            input = Vector2.zero;
+            _input = Vector2.zero;
     }
     
     public virtual void OnPointerUp(PointerEventData eventData)
     {
-        input = Vector2.zero;
-        handle.anchoredPosition = Vector2.zero;
+        _input = Vector2.zero;
+        _handle.anchoredPosition = Vector2.zero;
     }
 
     protected Vector2 ScreenPointToAnchoredPosition(Vector2 screenPosition)
     {
-        Vector2 localPoint = Vector2.zero;
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(baseRect, screenPosition, cam, out localPoint))
+        Vector2 localPoint;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_baseRect, screenPosition, _cam, out localPoint))
         {
-            Vector2 pivotOffset = baseRect.pivot * baseRect.sizeDelta;
-            return localPoint - (background.anchorMax * baseRect.sizeDelta) + pivotOffset;
+            Vector2 pivotOffset = _baseRect.pivot * _baseRect.sizeDelta;
+            return localPoint - (_background.anchorMax * _baseRect.sizeDelta) + pivotOffset;
         }
         return Vector2.zero;
     }
